7067e8f195631661af6b24f75cea8f038c181945
Vladimir Petrigo <taenaru@gmail.com>
2017-08-26 14:11:48 +0300

    Add toolchain triple and prefix variables for clang

--- clang-arm-gcc-toolchain.cmake
+++ clang-arm-gcc-toolchain.cmake
@@ -7,15 +7,18 @@ elseif(UNIX OR APPLE)
     set(UTIL_SEARCH_CMD which)
 endif()
 
+set(TOOLCHAIN_PREFIX arm-none-eabi-)
+set(TOOLCHAIN_TRIPLE arm-none-eabi)
+
 execute_process(
-        COMMAND ${UTIL_SEARCH_CMD} arm-none-eabi-gcc
+        COMMAND ${UTIL_SEARCH_CMD} ${TOOLCHAIN_PREFIX}gcc
         OUTPUT_VARIABLE BINUTILS_PATH
         OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 get_filename_component(ARM_TOOLCHAIN_DIR ${BINUTILS_PATH} DIRECTORY)
-set(triple arm-none-eabi)
-set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(triple ${TOOLCHAIN_TRIPLE})
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER clang++)
@@ -26,10 +29,10 @@ set(CMAKE_CXX_FLAGS_INIT "-B${ARM_TOOLCHAIN_DIR}")
 # only for successful compilation of CMake test
 set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
 # provide clang with ARM GCC toolchain include directory info
-include_directories(${ARM_TOOLCHAIN_DIR}/../arm-none-eabi/include)
+include_directories(${ARM_TOOLCHAIN_DIR}/../${TOOLCHAIN_TRIPLE}/include)
 
-set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/arm-none-eabi-objcopy CACHE INTERNAL "objcopy tool")
-set(CMAKE_SIZE_UTIL ${ARM_TOOLCHAIN_DIR}/arm-none-eabi-size CACHE INTERNAL "size tool")
+set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
+set(CMAKE_SIZE_UTIL ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
 
 set(CMAKE_FIND_ROOT_PATH ${ARM_TOOLCHAIN_DIR})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
