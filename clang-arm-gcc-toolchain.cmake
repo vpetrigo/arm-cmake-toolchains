21be3de1b102e6788d36329c0850464f357dbe57
Vladimir Petrigo <taenaru@gmail.com>
2019-02-03 19:48:47 +0300

    clang: Update Clang toolchain file
    Switch to:
     - llvm-objcopy for preparing binary images
     - llvm-size for determining memory occupation
    
    Address: issue #1

--- clang-arm-gcc-toolchain.cmake
+++ clang-arm-gcc-toolchain.cmake
@@ -24,19 +24,19 @@ set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
-set(CMAKE_C_FLAGS_INIT "-B${ARM_TOOLCHAIN_DIR}")
-set(CMAKE_CXX_FLAGS_INIT "-B${ARM_TOOLCHAIN_DIR}")
+set(CMAKE_C_FLAGS_INIT " -B${ARM_TOOLCHAIN_DIR} --sysroot=${ARM_TOOLCHAIN_DIR}/../lib/gcc --gcc-toolchain=${ARM_TOOLCHAIN_DIR}/.. ")
+set(CMAKE_CXX_FLAGS_INIT " -B${ARM_TOOLCHAIN_DIR} ")
 # Without that flag CMake is not able to pass test compilation check
 if (${CMAKE_VERSION} VERSION_EQUAL "3.6.0" OR ${CMAKE_VERSION} VERSION_GREATER "3.6")
     set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 else()
-    set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib")
 endif()
 # provide clang with ARM GCC toolchain include directory info
 include_directories(${ARM_TOOLCHAIN_DIR}/../${TOOLCHAIN_TRIPLE}/include)
 
-set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
-set(CMAKE_SIZE_UTIL ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
+set(CMAKE_OBJCOPY llvm-objcopy CACHE INTERNAL "objcopy tool")
+set(CMAKE_SIZE_UTIL llvm-size CACHE INTERNAL "size tool")
 
 set(CMAKE_FIND_ROOT_PATH ${ARM_TOOLCHAIN_DIR})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
