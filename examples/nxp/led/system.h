2ab6b742eff2753447a4b8976af64e42ab6ff46b
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-04-04 23:15:20 +0300

    nxp: k64f: Add intermediate system interface

---
+++ examples/nxp/led/system.h
@@ -0,0 +1,12 @@
+/**
+ * \file
+ * \brief
+ * \author Vladimir Petrigo
+ */
+
+#ifndef K64F_LED_SYSTEM_H
+#define K64F_LED_SYSTEM_H
+
+unsigned SystemCoreClockGet(void);
+
+#endif /* K64F_LED_SYSTEM_H */
